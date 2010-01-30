2bae65fba65ed2368560771959f48090cbf74d0e
?ukasz J?dral <borzole@gmail.com>
2010-01-30 20:10:07 +0100

    --

---
+++ xdg-rpm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+usage(){
+	echo -e "${0##*/} <paczka>
+	
+INFO:
+	-- Szybki podgląd plików z paczki rpm
+	Używa xdg-open, aby otworzyć/wykonać plik z paczki
+
+PRZYKŁAD:
+	-- wszystkie pliki z paczki
+	${0##*/} yum
+	-- z rozszerzeniem 'py'
+	${0##*/} yum py
+	
+by borzole.one.pl
+"
+	exit 0
+}
+[ $# = 0 ] && usage
+# ------------------------------------------------------------------------------
+APPS=$1
+TYPE=$2
+
+format(){
+	[ -z "$TYPE" ] && FILES=$(rpm -ql $APPS) || FILES=$(rpm -ql $APPS | grep "\.$TYPE$" )
+	
+	for p in $FILES; do 
+		echo "FALSE $p"
+	done
+}
+menu() {
+	zenity --title="${0##*/}" --text "Wybierz plik z <b>$APPS</b>" \
+		--width=600 --height=800 \
+		--list  --checklist \
+		--column="zaznacz" --column "plik" \
+		$(format) \
+		--separator " "  --multiple \
+		--print-column=2
+}	
+
+for plik in $(menu) ; do
+	xdg-open $plik
+done
