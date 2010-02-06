099c518f9363fd2e98bc8884e8bc5361341d3e38
?ukasz J?dral <borzole@gmail.com>
2010-02-06 14:41:55 +0100

    --

---
+++ pid-tlo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo PPID: $PPID
+echo PID: $$
+
+zenity --info --text "Jestem $$, czy $! 
+nie klikaj, zaraz mnie zabije: $PPID" \
+	1> ~/myscript.out.log 2> ~/myscript.err.log &
+TLO=$!
+sleep 2
+echo TLO: $TLO
+kill -TERM $TLO
+# lub 
+# kill -s SIGTERM $TLO
