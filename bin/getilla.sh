cb4d84e0cfe5afb19759ee6ff6ceb3ce03866fd3
?ukasz J?dral <borzole@gmail.com>
2010-03-23 15:57:53 +0100

    lab

--- bin/getilla.sh
+++ bin/getilla.sh
@@ -1,69 +1,32 @@
 #!/bin/bash
 
-#	{ wget | proz | aria2c | curl } z powiadomieniem
-#
-# by Michał Bentkowski (mr.ecik@gmail.com)
-# rozbudowane by borzole.one.pl
-# licensed under GPLv2+
-# requires: notify-send ( libnotify.rpm )
+# polecenie z powiadomieniem
  
-# Jako "download manager"
-# Wtyczka Flashgot do firefox:
-# PRZYKŁAD:
+# PRZYKŁAD: wtyczka Flashgot do firefox:
 # 	polecenie:
-#			/usr/bin/gnome-terminal
+#			/usr/bin/xterm
 # 	parametry:
-# 		-x getilla --wget [URL] -P [FOLDER]
-
-# ICONS: (change if you don't like them ;-))
-ICON_OK=messagebox_info
-ICON_ERR=messagebox_critical
-
+# 		-e getilla.sh wget -x [URL] -P [FOLDER]
+# 		-e getilla.sh proz [URL] -P [FOLDER]
+#
+# aria2c # nie używam :)
+# curl -C - -O # nie wiem, też nie używam ;P
 # ------------------------------------------------------------------------------
-# PATHS: (change them if it's needed)
-if [ "$#" -eq 0 ]; then
-	exit 1;
-elif [ "$1" = -w ]||[ "$1" = "--wget" ]; then
-	GET=/usr/bin/wget
-	# getilla --wget -x [URL] -P [FOLDER]
-elif [ "$1" = -p ]||[ "$1" = "--proz" ]; then
-	GET=/usr/bin/proz
-	# --proz [URL] -P [FOLDER]
-	# getilla --proz [URL] -P [FOLDER]
-elif [ "$1" = -a ]||[ "$1" = "--aria2c" ]; then
-	GET=/usr/bin/aria2c   
-	# nie używam :)
-elif [ "$1" = -c ]||[ "$1" = "--curl" ]; then
-	GET=/usr/bin/curl -C - -O
-	# nie wiem, też nie używam ;P
-else
-	GET=/usr/bin/?
-	shift $#
-fi
-
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 # ------------------------------------------------------------------------------
-# just run "get" with given parameters
-
-# $GET "${@:2}"
-shift
-$GET $@
-EXIT=$?
-
+[ $# -eq 0 ] && exit 1;
+$@
+RC=$?
 # check whether $DISPLAY exists,
 # if not, exit with get's exit code
-[ "x$DISPLAY" == "x" ] && exit $EXIT
+#~ [ "x$DISPLAY" == "x" ] && exit $EXIT
 
-# don't notify if --help
-# was invoked
-for p in $*; do [[ "$p" == "--help" ]] && exit $EXIT; done
-
-if [ "x$EXIT" == "x0" ]; then
-  notify-send -i $ICON_OK -u low "Download OK" "${*:2}"
-  #~ xdg-open ${*:2}
+if [[ $RC == 0 ]] ; then
+  notify-send -i messagebox_info -u low "OK" "${*:2}"
 else
-  notify-send -i $ICON_ERR -u critical "Download ERROR" "$*"
+  notify-send -i messagebox_critical -u critical "ERROR" "$*"
 fi
-exit $EXIT
+exit $RC
 
 
 
