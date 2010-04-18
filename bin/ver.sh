5880ba184db568aa5dd19a732ca97c455bdeb1d8
?ukasz J?dral <borzole@gmail.com>
2010-04-18 11:32:05 +0200

    /usr/local/bin/ver.sh

--- bin/ver.sh
+++ bin/ver.sh
@@ -5,11 +5,10 @@ usage(){
 	exit 0
 }
 
-[ $# -eq 0 ] && usage
-[ $# -eq 2 ] && MSG="-m'$2'" || MSG=""
+[[ $# -eq 0 ]] && usage
+[[ ! -f $1 ]] && usage
+[[ $# -eq 2 ]] && MSG="-m'$2'" || MSG=""
 
 [[ ! -d RCS ]] && mkdir RCS
-
-[[ ! -f $1 ]] && usage
 
 ci "$MSG" "$1" && co -l "$1"
