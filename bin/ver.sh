0b1009992dd9fe5ddc92e67c89930c591a0d8cb3
?ukasz J?dral <borzole@gmail.com>
2010-04-18 11:15:41 +0200

    /usr/local/bin/ver.sh

--- bin/ver.sh
+++ bin/ver.sh
@@ -2,7 +2,4 @@
 
 [ $# -eq 0 ] && { echo "Użycie: ${0##*/} plik" ; exit 0 ; }
 [ ! -d ./RCS ] && mkdir RCS
-#~ ci "$1" && co -l "$1",v
-ci -u "$1"
-chmod +w "$1"
-co -l "$1",v
+ci "$1" && co -l "$1",v
