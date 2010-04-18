350fd4e2b515042e43fd4a52880e183307a62754
?ukasz J?dral <borzole@gmail.com>
2010-04-18 11:26:50 +0200

    /usr/local/bin/ver.sh

--- bin/ver.sh
+++ bin/ver.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-[ $# -eq 0 ] && { echo "Użycie: ${0##*/} plik" ; exit 0 ; }
+usage(){
+	echo "Użycie: ${0##*/} plik"
+	exit 0
+}
+
+[ $# -eq 0 ] && usage
 [ $# -eq 2 ] && MSG="-m'$2'" || MSG=""
 
 [ ! -d RCS ] && mkdir RCS
 
 RV="RCS/${1},v"
 
-[ ! -f "$1" ] && [ -f ./RCS/"$1",v ]
+[ ! -f "$1" ] && { [ -f ./RCS/"$1",v ] && co -l "$RV" || usage ; }
 
 ci "$MSG" "$1" && co -l "$RV"
