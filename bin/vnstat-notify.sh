bba75d227bc0ea663b880a1535daff9933e5db5b
?ukasz J?dral <borzole@gmail.com>
2010-04-26 11:01:37 +0200

    /usr/local/bin/vnstat-notify.sh

--- bin/vnstat-notify.sh
+++ bin/vnstat-notify.sh
@@ -5,4 +5,27 @@ msg(){
 	vnstat -i ppp0
 }
 
-notify-send "VNSTAT" "$(msg)"
+ICON=/usr/share/icons/Fedora/scalable/apps/anaconda.svg
+
+# NotifyOSD: http://forums.fedoraforum.org/showthread.php?t=225028
+#~ ICON=notification-audio-next
+#~ ICON=notification-audio-play
+#~ ICON=notification-audio-previous
+#~ ICON=notification-audio-volume-high
+#~ ICON=notification-audio-volume-low
+#~ ICON=notification-audio-volume-medium
+#~ ICON=notification-audio-volume-muted
+#~ ICON=notification-audio-volume-off
+#~ ICON=notification-battery-low
+#~ ICON=notification-device-eject
+#~ ICON=notification-device
+#~ ICON=notification-display-brightness
+#~ ICON=notification-keyboard-brightness
+#~ ICON=notification-message-email
+#~ ICON=notification-message-im
+#~ ICON=notification-network-ethernet-connected
+#~ ICON=notification-network-ethernet-diconnected
+#~ ICON=notification-network-wireless
+#~ ICON=notification-power
+
+notify-send -i "$ICON" "VNSTAT" "$(msg)"
