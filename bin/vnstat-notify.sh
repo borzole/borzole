0b6025655066e49b70c6c62760c4b2a22eb22797
?ukasz J?dral <borzole@gmail.com>
2010-04-26 11:42:46 +0200

    /usr/local/bin/vnstat-notify.sh

--- bin/vnstat-notify.sh
+++ bin/vnstat-notify.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
+title(){
+	echo -n "vnstat - "
+	vnstat -i ppp0 | grep ^Database | sed -e 's/:/\n\t/'
+}
+
 msg(){
-	#~ vnstat | grep '/' | sed -e 's/today//g'
-	vnstat -i ppp0
+	vnstat -i ppp0 | grep -v ^Database
 }
 
 ICON=/usr/share/icons/Fedora/scalable/apps/anaconda.svg
 
-# NotifyOSD: http://forums.fedoraforum.org/showthread.php?t=225028
-#~ ICON=notification-audio-next
-#~ ICON=notification-audio-play
-#~ ICON=notification-audio-previous
-#~ ICON=notification-audio-volume-high
-#~ ICON=notification-audio-volume-low
-#~ ICON=notification-audio-volume-medium
-#~ ICON=notification-audio-volume-muted
-#~ ICON=notification-audio-volume-off
-#~ ICON=notification-battery-low
-#~ ICON=notification-device-eject
-#~ ICON=notification-device
-#~ ICON=notification-display-brightness
-#~ ICON=notification-keyboard-brightness
-#~ ICON=notification-message-email
-#~ ICON=notification-message-im
-#~ ICON=notification-network-ethernet-connected
-#~ ICON=notification-network-ethernet-diconnected
-#~ ICON=notification-network-wireless
-#~ ICON=notification-power
-
-# http://www.galago-project.org/specs/notification/0.9/index.html
 notify-send -u low \
-			-t 4000 \
+			-t 0 \
 			-i $ICON \
-			"VNSTAT" \
+			"$(title)" \
 			"$(msg)"
-
