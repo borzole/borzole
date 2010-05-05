dd21ecb64951dcdaafe36c3a37c437796ff9f5e8
?ukasz J?dral <borzole@gmail.com>
2010-05-05 20:22:36 +0200

    /usr/local/share/bslib/msg/yes_no.sh

--- share/bslib/msg/yes_no.sh
+++ share/bslib/msg/yes_no.sh
@@ -4,17 +4,18 @@ yes_no(){
 	# @parametr: krótki opis wyboru
 	# @return: 0|1
 	# np:	yesno "Zainstalować nowy kernel?"
-	echo -e "[ yes/no ] $@"
+	local YN="[ yes/no ] $@"
+	echo -e "$YN"
 	local x=''
 	while : ; do
 		read x
-		case "$x" in 
+		case "$x" in
 			[tT] | [tT][aA][kK] | [yY] | [yY][eE][sS] )
 				return 0 ;;
 			[nN] | [nN][iI][eE] | [nN][oO] )
 				return 1 ;;
 			* )
-				echo " Wybierz yes/no" ;;
+				echo -e "$YN" ;;
 		esac
 	done
 }
