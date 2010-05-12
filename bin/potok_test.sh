5f3a493004b4806c5962b64f9636a1c410555a3a
?ukasz J?dral <borzole@gmail.com>
2010-05-12 13:42:40 +0200

    /usr/local/bin/potok_test.sh

--- bin/potok_test.sh
+++ bin/potok_test.sh
@@ -6,3 +6,15 @@ przezroczyste,\nżadne" | potok.sh
 potok.sh < potok_test.sh
 potok.sh
 
+#~ # silnik.sh
+#~ readlink /proc/$$/fd/0
+#~
+#~
+#~ echo $$
+#~ echo NIC | silnik.sh
+#~ silnik.sh < ~/.bashrc
+#~ silnik.sh <<__EOF__
+#~ silnik.sh <<$$
+#~ __EOF__
+#~ silnik.sh <<<$$
+#~ silnik.sh NIC
