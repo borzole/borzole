06d638974585ad323af1f73433d96ec465977479
?ukasz J?dral <borzole@gmail.com>
2010-05-19 23:35:37 +0200

    /usr/local/bin/rpm-instaled.sh

--- bin/rpm-instaled.sh
+++ bin/rpm-instaled.sh
@@ -2,6 +2,6 @@
 
 DATA=$(date +%Y.%m.%d-%H:%M)
 SYSTEM=$(sed -e 's/release\ *//g' /etc/system-release)
-LOG="$HOME/zainstalowane -- ${SYSTEM} $(arch) -- ${DATA}.log"
+LOG="$HOME/zainstalowane-${SYSTEM}-$(arch)-${DATA}.log"
 
 rpm -qa --qf "%{name}.%{arch} \n" | sort -u > "$LOG"
