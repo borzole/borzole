4d3b4265877171660766f12dac6bcded79f2bf9d
?ukasz J?dral <borzole@gmail.com>
2010-05-26 15:50:40 +0200

    /usr/local/bin/audacious-rand.sh

---
+++ bin/audacious-rand.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# audacious-rand.sh -- losuje piosenki do playlisty
+# wersja:2010.05.26-15:16
+# autor: borzole (jedral.one.pl)
+
+# ------------------------------------------------------------------------------
+[[ ${BASH_VERSINFO[0]} -ge 4 ]] || exit 1
+shopt -s globstar
+
+# folder z obrazkami: domyślny lub z parametru
+[ $# -eq 0 ] && DIR=$HOME/studio/art/music || DIR="$@"
+# ------------------------------------------------------------------------------
+get_nr(){
+	# "OK" - zwraca ile piosenek będzie losowane
+	# "Anuluj" - koniec skryptu
+	zenity --scale --title="${0##*/}" --width=300 \
+		--text="Ile piosenek wylosować panie?" \
+		--value=100 --min-value=5 --max-value=500 --step=5
+}
+# ------------------------------------------------------------------------------
+get_files(){
+	# zwraca wylosowaną listę
+	for p in $DIR/**/*.mp3 ; do
+		echo $p
+	done 2>/dev/null | shuf -n$NR
+}
+# ------------------------------------------------------------------------------
+daemon(){
+	# włączy odtwarzanie jak tylko znajdzie coś na liście,
+	# więc nie trzeba czekać na załadowanie całej listy.
+	while : ; do
+		if [ $(audtool2 playlist-length) -gt 0 ] ; then
+			audacious2 -p &
+			exit $?
+		else
+			sleep 0.1
+			$FUNCNAME
+		fi
+	done
+}
+# ------------------------------------------------------------------------------
+NR=$(get_nr) || exit 0
+# sprawdzamy czy działa audacious2, jeśli nie to uruchamiamy
+# bez tego nie działają narzędzia "audtool2"
+pidof audacious2 >/dev/null 2>&1 || {
+	audacious2 &
+	# wymagane jest drobne opóźnienie
+	sleep 0.1
+}
+# aktywujemy UI ?
+audtool2 activate
+# wyczyść playlistę
+audtool2 playlist-clear
+# włącz demona
+daemon &
+# powolne zapełnianie playlisty
+get_files | while read -r LINE ; do
+	audtool2 playlist-addurl "file://${LINE}"
+	sleep 0.05 # a tak, co by nie przemęczać systemu
+done
+
+
+# ------------------------------------------------------------------------------
