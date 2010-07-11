0c956222ca219ebb84d82050b5375e568058ddb4
?ukasz J?dral <borzole@gmail.com>
2010-07-11 21:42:47 +0200

    /usr/local/bin/indexer.py

--- bin/indexer.py
+++ bin/indexer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 #
-#       indexer.py
-#       Version     0.2
-#       Date        2010.05.02-08:49
+#	   indexer.py
+#	   Version	 0.2
+#	   Date		2010.05.02-08:49
 #
-#       Copyright 2009 AJ Hekman
-#       This work is licensed under the
-#       Creative Commons Attribution 3.0 United States License
+#	   Copyright 2009 AJ Hekman
+#	   This work is licensed under the
+#	   Creative Commons Attribution 3.0 United States License
 #
 #		Zmiany by Borzole:
 #		- ikonki na base64 (szybsza odpowiedź serwera)
@@ -35,64 +35,64 @@ else:
 
 # Funkcje
 def ScanDirectory(dirToScan):
-    d=[]
-    f=[]
-    for n in os.listdir(dirToScan):
-        if os.path.isfile(os.path.join(dirToScan, n)):
-            f.append(n)
-        else:
-            d.append(n)
-            ScanDirectory(os.path.join(dirToScan, n))
-    d.sort()
-    f.sort()
-    makeIndexHTML(dirToScan, d, f)
+	d=[]
+	f=[]
+	for n in os.listdir(dirToScan):
+		if os.path.isfile(os.path.join(dirToScan, n)):
+			f.append(n)
+		else:
+			d.append(n)
+			ScanDirectory(os.path.join(dirToScan, n))
+	d.sort()
+	f.sort()
+	makeIndexHTML(dirToScan, d, f)
 
 def makeIndexHTML(localDirectory, listofDirectories, listofFiles):
-    print localDirectory
+	print localDirectory
 
-    #do not index current directory if do_not_index is present
-    for n in listofFiles:
-        if n == "do_not_index": return
+	#do not index current directory if do_not_index is present
+	for n in listofFiles:
+		if n == "do_not_index": return
 
-    indexf = open(os.path.join(localDirectory,"index.html"), "w")
+	indexf = open(os.path.join(localDirectory,"index.html"), "w")
 
-    def parentcheck():
-            #if this is the directory the script was run from, do not make link point to a parent directory
-            if localDirectory == os.getcwd():
+	def parentcheck():
+			#if this is the directory the script was run from, do not make link point to a parent directory
+			if localDirectory == os.getcwd():
 				return "../index.html"
-            #if a parent directoy contains "do_not_index", do not link to it
-            elif os.path.exists(os.path.join(os.path.dirname(localDirectory),"do_not_index")):
+			#if a parent directoy contains "do_not_index", do not link to it
+			elif os.path.exists(os.path.join(os.path.dirname(localDirectory),"do_not_index")):
 				return ""
-            #if all else is good, return a valid link to parent directory
-            else:
+			#if all else is good, return a valid link to parent directory
+			else:
 				return "../index.html"
 
-    indexf.write(htmlHead % parentcheck() )
+	indexf.write(htmlHead % parentcheck() )
 
-    for n in listofDirectories:
-        #do not make link for child directory if do_not_index is present there
-        childDirectory = os.path.join(localDirectory, n)
-        if  os.path.exists(os.path.join(childDirectory, "do_not_index")): continue
-        indexf.write(htmlDir % (n, n))
+	for n in listofDirectories:
+		#do not make link for child directory if do_not_index is present there
+		childDirectory = os.path.join(localDirectory, n)
+		if  os.path.exists(os.path.join(childDirectory, "do_not_index")): continue
+		indexf.write(htmlDir % (n, n))
 
-    for n in listofFiles:
-        #do not list the following files, you may comment the next line out if you want them listed
-        if n == "index.html" or n == "indexer.pyc" or n == ".dropbox": continue
-        indexf.write(htmlFile % (n, n))
+	for n in listofFiles:
+		#do not list the following files, you may comment the next line out if you want them listed
+		if n == "index.html" or n == "indexer.pyc" or n == ".dropbox": continue
+		indexf.write(htmlFile % (n, n))
 
-    #write footer portion
-    indexf.write(htmlFoot % str(datetime.date.today()))
+	#write footer portion
+	indexf.write(htmlFoot % str(datetime.date.today()))
 
-    indexf.close()
+	indexf.close()
 
 htmlHead = """<html>
-    <head>
-        <title>dropbox :: folder listing</title>
-        <link rel="shortcut icon" href="http://www.getdropbox.com/static/1238803391/images/favicon.ico">
+	<head>
+		<title>dropbox :: folder listing</title>
+		<link rel="shortcut icon" href="http://www.getdropbox.com/static/1238803391/images/favicon.ico">
 <!--
-        <link type="text/css" rel="stylesheet" href="http://dl.dropbox.com/u/409786/web/css/indexpy.css">
+		<link type="text/css" rel="stylesheet" href="http://dl.dropbox.com/u/409786/web/css/indexpy.css">
 -->
-        <style type="text/css"><!--
+		<style type="text/css"><!--
 
 body, td {
 	font-family:lucida grande, lucida sans unicode, verdana;
@@ -124,36 +124,36 @@ a:visited:hover { color:#A52A2A; text-decoration:underline; }
 .dir  { background-image:url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAGrSURBVDjLxZO7ihRBFIa/6u0ZW7GHBUV0UQQTZzd3QdhMQxOfwMRXEANBMNQX0MzAzFAwEzHwARbNFDdwEd31Mj3X7a6uOr9BtzNjYjKBJ6nicP7v3KqcJFaxhBVtZUAK8OHlld2st7Xl3DJPVONP+zEUV4HqL5UDYHr5xvuQAjgl/Qs7TzvOOVAjxjlC+ePSwe6DfbVegLVuT4r14eTr6zvA8xSAoBLzx6pvj4l+DZIezuVkG9fY2H7YRQIMZIBwycmzH1/s3F8AapfIPNF3kQk7+kw9PWBy+IZOdg5Ug3mkAATy/t0usovzGeCUWTjCz0B+Sj0ekfdvkZ3abBv+U4GaCtJ1iEm6ANQJ6fEzrG/engcKw/wXQvEKxSEKQxRGKE7Izt+DSiwBJMUSm71rguMYhQKrBygOIRStf4TiFFRBvbRGKiQLWP29yRSHKBTtfdBmHs0BUpgvtgF4yRFR+NUKi0XZcYjCeCG2smkzLAHkbRBmP0/Uk26O5YnUActBp1GsAI+S5nRJJJal5K1aAMrq0d6Tm9uI6zjyf75dAe6tx/SsWeD//o2/Ab6IH3/h25pOAAAAAElFTkSuQmCC");}
 .file { background-image:url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAC4SURBVCjPdZFbDsIgEEWnrsMm7oGGfZrohxvU+Iq1TyjU60Bf1pac4Yc5YS4ZAtGWBMk/drQBOVwJlZrWYkLhsB8UV9K0BUrPGy9cWbng2CtEEUmLGppPjRwpbixUKHBiZRS0p+ZGhvs4irNEvWD8heHpbsyDXznPhYFOyTjJc13olIqzZCHBouE0FRMUjA+s1gTjaRgVFpqRwC8mfoXPPEVPS7LbRaJL2y7bOifRCTEli3U7BMWgLzKlW/CuebZPAAAAAElFTkSuQmCC");}
 
-        --></style>
-    </head>
-    <body><center>
-        <table border="0" cellspacing="0" cellpadding="2" width="800" >
-            <tr><td>
-                <a href="http://www.getdropbox.com/">
-                    <img border="0" alt="dropbox" src="http://www.getdropbox.com/static/images/main_logo.png">
-                </a>
-                <hr size="1" color="#c0c0c0">
-            </td></tr>
-            <tr><td><b>folder contents:</b></td></tr>
-            <tr><td><a class="up" href="%s">parent directory</a></td></tr>"""
+		--></style>
+	</head>
+	<body><center>
+		<table border="0" cellspacing="0" cellpadding="2" width="800" >
+			<tr><td>
+				<a href="http://www.getdropbox.com/">
+					<img border="0" alt="dropbox" src="http://www.getdropbox.com/static/images/main_logo.png">
+				</a>
+				<hr size="1" color="#c0c0c0">
+			</td></tr>
+			<tr><td><b>folder contents:</b></td></tr>
+			<tr><td><a class="up" href="%s">parent directory</a></td></tr>"""
 
 htmlDir = """
-            <tr><td><a class="dir" href="%s/index.html">%s</a></td></tr>"""
+			<tr><td><a class="dir" href="%s/index.html">%s</a></td></tr>"""
 
 htmlFile = """
-            <tr><td><a class="file" href="%s">%s</a></td></tr>"""
+			<tr><td><a class="file" href="%s">%s</a></td></tr>"""
 
 htmlFoot = """
-            <tr><td>
-              <hr size="1" color="#c0c0c0">
-              <font color="#808080">generated on %s</font>
-            </td></tr>
-            <tr><td align="right">
-              <img border="0" alt="dropbox" src="data:image/gif;base64,R0lGODlhFAAUAOZNAMrKyvLy8urq6sfHx6mpqe3t7aenp+jo6NTU1MjIyKioqNjY2MzMzNzc3Pb29qysrM7OztPT0+Xl5aWlpdLS0u/v7/T09Nra2snJyba2tqurq9vb297e3vr6+sHBwfDw8MLCwrOzs7CwsL29vbm5ua+vr5qamsDAwOLi4ri4uL+/v7y8vPPz8/n5+aGhoaSkpL6+vp6enuHh4bW1tcbGxqKiotfX17GxsZ+fn6Ojo9nZ2d/f37e3t8/Pz8TExIiIiPX19a6urs3NzdbW1rKysuvr68vLy6qqqsPDw8XFxaampvj4+NHR0fv7+wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAE0ALAAAAAAUABQAAAfugE2Cg4SFhoeIiRZBBi2DRwoBiRs5DEwagg8wDAoIhkshBEwXKDYGLiQUCxwjIkCEJjNMTAgXEjIDAwAUDQcbNYQAMZYRGwUdFjtCPQ0SJDSFOjgAHAVLSwEWBRwLJUyHCQ8VSw5LHwEdSzArhwsTICAH1wEOAgBMPBCFSS8QAyUeUGBjQuAGAgkqThCaIILCACIeMBwY8MNAiggChigg1GKEAh8hkCwQcMIAgRQIVmRwYOiCiQQMBFQAkCBDAg3fEL1gcqBCgQEQGkRQkqgJCwIAzt1ToYFFUUEYMgjA4MLIU0JFHkwQcLWr10SBAAA7">
-              <font color="#808080">&copy; 2009 dropbox</font>
-            </td></tr>
-        </table>
-    </body>
+			<tr><td>
+			  <hr size="1" color="#c0c0c0">
+			  <font color="#808080">generated on %s</font>
+			</td></tr>
+			<tr><td align="right">
+			  <img border="0" alt="dropbox" src="data:image/gif;base64,R0lGODlhFAAUAOZNAMrKyvLy8urq6sfHx6mpqe3t7aenp+jo6NTU1MjIyKioqNjY2MzMzNzc3Pb29qysrM7OztPT0+Xl5aWlpdLS0u/v7/T09Nra2snJyba2tqurq9vb297e3vr6+sHBwfDw8MLCwrOzs7CwsL29vbm5ua+vr5qamsDAwOLi4ri4uL+/v7y8vPPz8/n5+aGhoaSkpL6+vp6enuHh4bW1tcbGxqKiotfX17GxsZ+fn6Ojo9nZ2d/f37e3t8/Pz8TExIiIiPX19a6urs3NzdbW1rKysuvr68vLy6qqqsPDw8XFxaampvj4+NHR0fv7+wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAE0ALAAAAAAUABQAAAfugE2Cg4SFhoeIiRZBBi2DRwoBiRs5DEwagg8wDAoIhkshBEwXKDYGLiQUCxwjIkCEJjNMTAgXEjIDAwAUDQcbNYQAMZYRGwUdFjtCPQ0SJDSFOjgAHAVLSwEWBRwLJUyHCQ8VSw5LHwEdSzArhwsTICAH1wEOAgBMPBCFSS8QAyUeUGBjQuAGAgkqThCaIILCACIeMBwY8MNAiggChigg1GKEAh8hkCwQcMIAgRQIVmRwYOiCiQQMBFQAkCBDAg3fEL1gcqBCgQEQGkRQkqgJCwIAzt1ToYFFUUEYMgjA4MLIU0JFHkwQcLWr10SBAAA7">
+			  <font color="#808080">&copy; 2009 dropbox</font>
+			</td></tr>
+		</table>
+	</body>
 </html>"""
 
 # Go!
