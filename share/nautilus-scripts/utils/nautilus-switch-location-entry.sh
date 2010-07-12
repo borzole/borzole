2964e69f861ab7bcfc0308356eaf5ba8b8a12a27
?ukasz J?dral <borzole@gmail.com>
2010-07-12 10:15:51 +0200

    zmiana lokalizacji nautilus-script

--- share/nautilus-scripts/utils/nautilus-switch-location-entry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# przełącznik paska adresu nautilusa: guziki/ścieżka
-
-cmd='gconftool-2 /apps/nautilus/preferences/always_use_location_entry'
-switch="$cmd --set --type=Boolean"
-
-[ $($cmd --get) = 'true' ] && $switch false || $switch true
