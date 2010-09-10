a30b6b56f0e36de5d27541b93b4633ec66ff45d7
?ukasz J?dral <borzole@gmail.com>
2010-09-10 16:33:21 +0200

    /usr/local/bin/firefox-opt.sh

--- bin/firefox-opt.sh
+++ bin/firefox-opt.sh
@@ -3,7 +3,7 @@
 # komunikaty
 
 killall firefox 2>>$LOG
-find $HOME/.mozilla/ -iname \*.sqlite -exec sqlite3  '{}' "vacuum" \; \
+find $HOME/.mozilla/ -iname \*.sqlite -exec sqlite3  '{}' "VACUUM" \; \
 	| zenity --progress --pulsate --auto-close --auto-kill
 
 firefox &
