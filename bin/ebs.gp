2bd72756728c9be31513d7e4d7f6a3dc339174e7
?ukasz J?dral <borzole@gmail.com>
2010-11-18 15:28:35 +0100

    /usr/local/bin/module.sh

--- bin/ebs.gp
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/gnuplot -persist
-
-binwidth=5
-bin(x,width)=width*floor(x/width)
-
-plot '/dev/shm/datafile' using (bin($1,binwidth)):(1.0) smooth freq with boxes
--- bin/module.sh
+++ bin/module.sh
@@ -7,7 +7,7 @@ exec 2> >( grep --color=always \. ) # trik koloruje błędy na czerwono
 
 list(){
         for p in $@ ; do
-                echo /usr/lib{,64}/python{2.6,3.1}/{,plat-linux2/,lib-tk/,lib-old/,lib-dynload/,site-packages/{,PIL/,gst-0.10/,gtk-2.0/,wx-2.8-gtk2-unicode/}}${p}{.py,/__init__.py}
+                echo /usr/lib{,64}/python{2.6,2.7,3.1}/{,plat-linux2/,lib-tk/,lib-old/,lib-dynload/,site-packages/{,PIL/,gst-0.10/,gtk-2.0/,wx-2.8-gtk2-unicode/}}${p}{.py,/__init__.py}
         done
 }
 
