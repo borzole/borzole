ba8f92bb58d136885cd4c97d31723802d7092d5f
?ukasz J?dral <borzole@gmail.com>
2010-11-20 17:26:25 +0100

    /usr/local/bin/ebs.py

--- bin/ebs.py
+++ bin/ebs.py
@@ -2,18 +2,18 @@
 #-*- coding:utf-8 -*-
 
 # ebs -- Evidence Based Scheduling
+# ver. 2010.11.20-17:24
+# autor: http://jedral.one.pl/2010/11/na-80-w-piatek.html
+
+# http://matplotlib.sourceforge.net/users/navigation_toolbar.html
 
 import random, sys,os
-
-import time
-
-import gobject
-import gtk
+import gobject, gtk
 
 import matplotlib
 matplotlib.use('GTKAgg')
-from matplotlib import pyplot, cm, colors
-from pylab import get_current_fig_manager as get_current_fig_manager
+from matplotlib import use, pyplot, cm, colors
+from pylab import get_current_fig_manager
 
 class Cli(object):
 	def __init__ (self):
@@ -50,18 +50,30 @@ class MonteCarlo(object):
 
 		self.u = sum(self.P)*(sum(self.E)*1.0)/len(self.E)
 
-	probe = lambda self: round( sum([ i*random.sample(self.E,1)[0] for i in self.P ]) , 2 )
+	probe = lambda self: round( sum([ i*random.choice(self.E) for i in self.P ]) , 2 )
 	probes = lambda self,n: [ self.probe() for i in range(n) ]
 
 class PlotEBS(object):
 	def __init__ (self,mc):
 		self.count = 50
+		self.scale = 3
 		self.mc = mc
 		self.u = mc.u
-		self.H = mc.probes(1000)
+		self.H = []
+		self.pause = False
+		self.help = 1
+		self.arrow = True
+		self.usage=['h - show/hide help',
+					'a - narrow bars',
+					'z - extend bars',
+					'x - narrow x ticks',
+					'c - extend x ticks',
+					'e - show arrow',
+					'q - pause',
+					'esc - exit']
 
 		self.figure = pyplot.figure()
-		self.ax = self.figure.add_subplot(111)
+		self.ax = self.figure.add_subplot(111) # (211)
 
 		self.histogram()
 		self.manager = get_current_fig_manager()
@@ -78,6 +90,14 @@ class PlotEBS(object):
 			print 'press', event.key
 			if event.key=='a': self.count /=0.9
 			if event.key=='z': self.count /=1.1
+			if event.key=='c': self.scale +=1
+			if event.key=='x' and self.scale !=1: self.scale -=1
+			if event.key=='e': self.arrow = not self.arrow
+			if event.key=='h':
+				if self.help == 2:
+						self.help = 0
+				else:
+					self.help += 1
 			if event.key=='q':
 				if self.pause:
 					self.pause = False
@@ -86,31 +106,44 @@ class PlotEBS(object):
 					self.pause = True
 			if event.key=='escape': gtk.main_quit()
 
-		self.pause = False
 		self.figure.canvas.mpl_connect('key_press_event', key_press)
 
 		gobject.idle_add(update)
 		pyplot.show()
 
 	def histogram(self):
-		self.H += self.mc.probes(1000)
-		self.ax.set_title('Histogram of Evidence Based Scheduling [ %s ]'%str(len(self.H)))
-		self.ax.set_xlabel('Time (h)')
-		self.ax.set_ylabel('Probability (%)')
+		self.ax.set_title('Histogram of Evidence Based Scheduling [ %d ]' % len(self.H))
+		self.ax.set_xlabel('Time (h)',fontstyle='italic')
+		self.ax.set_ylabel('Probability (%)',fontstyle='italic')
 		self.ax.set_ylim(0,110)
-
-		#@TODO nie mogę ustawić skali osi Y
-		# a, b = 2, 9 # integral area
-		# ax.set_xticks((a,b))
-		# ax.set_xticklabels(('a','b'))
-
 		self.ax.grid(True)
-		#@TODO label na średniej się nie pojawia
-		self.ax.axvline(self.u, color='#90EE90', linestyle='dashed', label=" simple estimation ", lw=2)
 
+		self.ax.axvline(self.u, color='#90EE90', linestyle='dashed', lw=2)
 
+		self.H += self.mc.probes(1000)
 		n, bins, patches = self.ax.hist(self.H, bins=self.count , edgecolor='white', alpha=0.75)
 		nmax=n.max() # najwyższy słupek
+
+		# zmienna skala osi Y
+		self.ax.set_xticks([ round(i,2) for i in bins[::self.scale] ])
+		# self.ax.set_xticklabels(('a','b')) # tak można dodać label zamiast wartości
+		self.figure.autofmt_xdate() # pochyłe literki
+
+		# strzałka
+		if self.arrow:
+			pyplot.annotate('simple estimation', xy=(self.u, 90), xytext=(min(bins), 100),
+				arrowprops=dict(facecolor='blue', shrink=0.005))
+
+		if self.help == 1 :
+			pyplot.annotate('help (h)',
+				xy=(max(bins), 90),
+				xytext=((self.u+max(bins))/2, 90),
+				ha='left')
+		elif self.help == 2 :
+			pyplot.annotate('\n'.join(self.usage),
+				xy=(max(bins), 90),
+				xytext=((self.u+max(bins))/2, 60),
+				ha='left')
 
 		# normalizacja
 		for p in patches:
@@ -124,9 +157,7 @@ class PlotEBS(object):
 			color = cm.jet(norm(f))
 			p.set_facecolor(color)
 
-
 if __name__ == '__main__':
 	cli = Cli()
 	mc = MonteCarlo(cli.project,cli.estimator)
-	#~ mc = MonteCarlo('/home/lucas/p.dat','/home/lucas/n.dat')
 	PlotEBS(mc)
