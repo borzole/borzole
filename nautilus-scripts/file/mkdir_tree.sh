401a3623bcaf934f610e788586b69e1a0ddfa51a
?ukasz J?dral <borzole@gmail.com>
2010-12-01 19:13:06 +0100

    /usr/local/share/nautilus-scripts/file/mkdir_tree.sh

---
+++ nautilus-scripts/file/mkdir_tree.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# make folders
+
+cd "${NAUTILUS_SCRIPT_CURRENT_URI#file://}"
+
+mkdir -p {lab,src,arch,class,doc,tmp}
+
+# README:
+# arch, src, lab -- source code
+#                -- lab: current project
+#                -- src: waiting, not finished projects
+#                -- arch: finished projects
+#          class -- templates
+#            doc -- documentation
+#            tmp -- inbox
