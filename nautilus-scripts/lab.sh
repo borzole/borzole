5d6d0f522d41b2c80c19d1842edfc601a6d0f606
?ukasz J?dral <borzole@gmail.com>
2010-12-04 00:34:34 +0100

    /usr/local/share/nautilus-scripts/lab.sh

--- nautilus-scripts/lab.sh
+++ nautilus-scripts/lab.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-r="${NAUTILUS_SCRIPT_CURRENT_URI#file://}"
-r="$1"
 # minimum Bash 4
 [[ ${BASH_VERSINFO[0]} -ge 4 ]] || exit 1
 
@@ -10,13 +8,8 @@ shopt -s globstar
 # enable ** for dotfiles
 shopt -s dotglob
 
-for p in "$r"/** ; do
-	if [[ -d $p ]] ; then
-		gvfs-set-attribute "$p" metadata::nautilus-icon-view-auto-layout true
-		gvfs-set-attribute "$p" metadata::nautilus-icon-view-tighter-layout false
-		gvfs-set-attribute "$p" metadata::nautilus-icon-view-zoom-level 3
-		gvfs-set-attribute "$p" metadata::nautilus-default-view OAFIID:Nautilus_File_Manager_Icon_View
-		gvfs-set-attribute "$p" -t stringv metadata::annotation
-		#@TODO działa jak się wejdzie poraz drugi do katalogu, ustawiany jest jeszcze jakiś parametr ?!
-	fi
+r=${1:-${NAUTILUS_SCRIPT_CURRENT_URI#file://}}
+
+for f in "$r"/** ; do
+
 done
